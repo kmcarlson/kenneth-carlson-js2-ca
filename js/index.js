867e1194d1ca44a54b3084133ccb76cffd36450f
Kenneth Carlson <carlson90@hotmail.com>
2023-01-10 16:05:42 +0100

    add more stuff

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 window.onload = function () {
   fetchApi();
+  const wishes = JSON.parse(localStorage.getItem("wish"));
+  const wishlist = document.getElementById("wishlist");
+  if (wishes.length > 0) {
+    wishlist.innerHTML = "";
+    console.log(wishes)
+    wishes.forEach((wish) => {
+      const item = document.createElement("li");
+      item.innerHTML = wish;
+      wishlist.appendChild(item);
+    });
+  }
+  console.log(wishes);
 };
 
 function fetchApi() {
@@ -17,7 +29,7 @@ function fetchApi() {
             <br>
             <span>${item.author}</span>
             <br>
-            <button onclick="toggleFavourite('${item.id}')">Favourite</button>
+            <button onclick="toggleFavourite('${item.id}')">Add to wishlist</button>
             <br>
          
         `;
@@ -26,4 +38,14 @@ function fetchApi() {
     });
 }
 
+function toggleFavourite(id) {
+  let wish = JSON.parse(localStorage.getItem("wish")) || [];
+  let index = wish.indexOf(id);
 
+  if (index === -1) {
+    wish.push(id);
+  } else {
+    wish.splice(index, 1);
+  }
+  localStorage.setItem("wish", JSON.stringify(wish));
+}
